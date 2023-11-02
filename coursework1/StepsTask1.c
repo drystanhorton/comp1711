e2708914c804e6f635054e9f2fb4afa9bcfb8ae3
drystanhorton <146240270+drystanhorton@users.noreply.github.com>
2023-11-02 03:08:33 +0000

    Update StepsTask1.c

--- coursework1/StepsTask1.c
+++ coursework1/StepsTask1.c
@@ -9,12 +9,18 @@ typedef struct {
 	int steps;
 } FITNESS_DATA;
 
+// Define any additional variables here
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
-    // create a copy of the input string as strtok modifies the string
+    // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     
-    // tokenize the copied string
+    // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
     if (token != NULL) {        strcpy(date, token);
     }
@@ -29,11 +35,53 @@ void tokeniseRecord(const char *input, const char *delimiter,
         strcpy(steps, token);
     }
     
-    // free the duplicated string
+    // Free the duplicated string
     free(inputCopy);
+
 }
 
-int main()
-{
-    
+int main() {
+// Open the csv file
+    FILE *file = fopen("FitnessData_2023.csv", "r");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Read the records
+    int recordCount = 0;
+    char buffer[100];  // Make room for maximum 100 characters per line
+
+    while (fgets(buffer, sizeof(buffer), file)) {
+        recordCount++;
+    }
+
+    // Print the number of records
+    printf("Number of records in file: %d\n", recordCount);
+
+    // 'Rewind' the file to the start
+    rewind(file);
+
+    // Create an array of 'FITNESS_DATA' to store the data
+    FITNESS_DATA data[recordCount];
+
+    // Read and store the data in the array 'data'
+    for (int i = 0; i < recordCount; i++) {
+        fgets(buffer, sizeof(buffer), file);
+        char date[11], time[6], steps[10];
+        tokeniseRecord(buffer, ",", date, time, steps);
+        strcpy(data[i].date, date);
+        strcpy(data[i].time, time);
+        data[i].steps = atoi(steps);
+    }
+
+    // Print the first three records
+    for (int i = 0; i < 3; i++) {
+        printf("%s/%s/%d\n", data[i].date, data[i].time, data[i].steps);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return 0;
 }
